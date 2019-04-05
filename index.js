0a26e1f79893a1a77f6a52897ae6bb26081b1b3a
Zoharos <zorgolan@gmail.com>
2019-04-05 15:23:53 +0300

    adapting to heroku

--- index.js
+++ index.js
@@ -13,22 +13,16 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-const envClasses = process.env.LIVE_CLASSES;
-const liveClasses = envClasses.split(",");
-
-const stdout = execSync('cp -u ' + __dirname + '/cloud/main.js /cloud');
-console.log(stdout);
-
 const api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: '/cloud/main.js', //process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  databaseURI: databaseUri || 'mongodb+srv://parse:fTIqjcegIsfRGVie@golancorporation-cyaxt.gcp.mongodb.net/test?retryWrites=true',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  // restAPIKey: process.env.REST_API_KEY || '',
+  restAPIKey: process.env.REST_API_KEY || '',
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  allowClientClassCreation: process.env.ALLOW_CLIENT_CLASS_CREATION === "true",
+  allowClientClassCreation: false,
   liveQuery: {
-    classNames: liveClasses // List of classes to support for query subscriptions
+    classNames: ["liveClasses "]// List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
@@ -39,8 +33,7 @@ const options = { allowInsecureHTTP: true };
 const dashboard = new ParseDashboard({
   apps: [
     {
-      serverURL: "https://parse-server-example-" + process.env.PROJECT_NAME + ".1d35.starter-us-east-1.openshiftapps.com/parse",
-      //"https://parse-server-example-" + process.env.PROJECT_NAME + ".7e14.starter-us-west-2.openshiftapps.com/parse",
+      serverURL: "https://realnadlanparseserver.herokuapp.com/parse",
       appId: process.env.APP_ID || "myAppId",
       masterKey: process.env.MASTER_KEY || "myMasterKey",
       appName: process.env.APP_NAME || "MyApp"
